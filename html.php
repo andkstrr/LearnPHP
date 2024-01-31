c60d75ce4db8b3cfe77bfc185e705679d61df25f
andkstrr <andikasatrionurcahyo@gmail.com>
2024-01-31 15:12:41 +0700

    first try

---
+++ html.php
@@ -0,0 +1,58 @@
+<?php
+    // $string = 'Ini adalah contoh string sederhana';
+    // $string2 = 'Variabel juga tidak otomatis ditampilkan : $string';
+    
+    // echo $string;
+    // echo "<br>";
+    // echo $string2;
+    
+    // echo "<br>";
+    
+    // $stringa = "Ini adalah string sederhana 2";
+    // $stringb = "Variabel akan otomatis ditampilkan : $string";
+    
+    // echo $stringa;
+    // echo "<br>";
+    // echo $stringb;
+    
+    // echo "<br>"; echo "<br>";
+    
+    // $nama = "Andika";
+    // $umur = 15;
+    
+    // echo "Nama saya adalah ", $nama, " dan saya berumur ", $umur; echo "<br>";
+    // echo "Nama saya adalah ". $nama. " dan saya berumur ". $umur; echo "<br>";
+    
+    // echo "<br>"; echo "<br>";
+    
+    // $a = "1";
+    // $b = 2;
+    // echo $a + $b;
+    
+            
+    
+    // $name = "Andikastrr";
+    // $age = 15;
+    // $score = 100.22;
+    
+    // var_dump($name);
+    // echo "<br>";
+    // var_dump($age);
+    // echo "<br>";
+    // var_dump($score);
+    
+            
+    
+    // ARRAY
+    // Cara 1 ()
+    // $laptops = array("asus", "acer", "huawei", "lenovo");
+    // var_dump($laptops)
+    
+    // Cara 2 []
+    // $laptops = ["asus", "acer", "huawei", "lenovo"];
+    // var_dump($laptops)
+    
+    // Cara hanya ambil 1 data menggunakan []; var_dump($laptops[1]) //
+    
+    // Cara 3
+?>
