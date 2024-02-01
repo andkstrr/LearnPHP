877f57212f5f98304efce62a6003d64b7f95208c
andkstrr <andikasatrionurcahyo@gmail.com>
2024-02-01 15:01:37 +0700

    day 2

---
+++ test.php
@@ -0,0 +1,30 @@
+<!-- <?php
+    // echo "Hello, World! <br>";
+    // echo "Andika Satrio Nurcahyo";
+    
+    // $name = "Andika <br>";
+    // echo "$name";
+    
+    // $no = 20;
+    // $nomer = 32;
+    
+    // $add = $no += 15;
+    // echo "$add <br>";
+    
+    // $num = 16;
+    // $num++;
+    // $num++;
+    // $num++;
+    // echo $num;
+    
+    // $waktuLembur = 5;
+    
+    // // 5 jam komisi 20000
+    // // <5 jam 10000
+    // // >5 jam 30000
+    
+    // $lembur = ($waktuLembur == 5) ? "Komisi 20000" : (($waktuLembur >= 5) ? "Komisi 30000" : (($waktuLembur <= 5) ? "Komisi 10000" : "Tidak dapat komisi"));
+    // echo "Jika lembur selama $waktuLembur, maka mendapat $lembur";
+    
+    
+?> -->
